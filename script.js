ea60561bad016d4f569826da0d27a4131f28f5e8
Wyatt Wilson <95758153+DeterioratingSanity@users.noreply.github.com>
2023-02-07 23:27:02 -0500

    homepage fin (pol 1)

---
+++ script.js
@@ -0,0 +1,46 @@
+$('.navOne').click(function(){
+  window.open('./products.html', '_self');
+  return false;
+});
+
+$('.navTwo').click(function(){
+  window.open('./reviews.html', '_self');
+  return false;
+});
+
+$('.navThree').click(function(){
+  window.open('./contact.html', '_self');
+  return false;
+});
+
+$('.navFive').click(function(){
+  window.open('./about.html', '_self');
+  return false;
+});
+$('.navH').click(function(){
+  window.open('./index.html', '_self');
+  return false;
+})
+
+$('#icon1').click(function(){
+  window.open('https://www.facebook.com/');
+  return false;
+})
+$('#icon2').click(function(){
+  window.open('https://www.instagram.com/');
+  return false;
+})
+$('#icon3').click(function(){
+  window.open('https://www.youtube.com/');
+  return false;
+})
+
+$('#ret').click(function(){
+  window.open('./index.html', '_self');
+  return false;
+})
+
+$('#headTitle').click(function(){
+  window.open('./index.html', '_self');
+  return false;
+})
